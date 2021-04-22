2f3cd339ed8fa2f9dace2d48b4a5907f2095c9e1
Jonathan St-Onge <35715881+jstonge@users.noreply.github.com>
2021-04-22 10:32:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
+#with open('README.rst') as readme_file:
+#    readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
+#with open('HISTORY.rst') as history_file:
+#    history = history_file.read()
 
 requirements = [ ]
 
